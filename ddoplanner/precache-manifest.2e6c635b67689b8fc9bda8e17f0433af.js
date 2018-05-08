8767703460ffaa1dc33e069fb88ee2c6b2922b30
taplar <taplarhw@gmail.com>
2018-05-07 20:14:39 -0500

    Include manifest file.

---
+++ ddoplanner/precache-manifest.2e6c635b67689b8fc9bda8e17f0433af.js
@@ -0,0 +1,10 @@
+self.__precacheManifest = [
+  {
+    "revision": "309742f38e862ad5da22",
+    "url": "main.js"
+  },
+  {
+    "revision": "0026c769854898b932cdb9e55e61e81a",
+    "url": "index.html"
+  }
+];
---
+++ ddoplanner/sw.js
@@ -0,0 +1,3 @@
+importScripts("precache-manifest.2e6c635b67689b8fc9bda8e17f0433af.js", "https://storage.googleapis.com/workbox-cdn/releases/3.2.0/workbox-sw.js");
+
+
