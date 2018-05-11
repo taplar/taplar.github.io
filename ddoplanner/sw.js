b5c2a1a65c9785b9c3990246ed26ea5919b01272
taplar <taplarhw@gmail.com>
2018-05-10 21:56:08 -0500

    Updated ddoplanner to revision 90bb6b52e554e34933ee9e02547b489eb1c7b67c

--- ddoplanner/sw.js
+++ ddoplanner/sw.js
@@ -12,6 +12,9 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
+workbox.routing.registerRoute(/^http[s]?:\/\/fonts.googleapis.com\/(.*)/, workbox.strategies.staleWhileRevalidate(), 'GET');
+workbox.routing.registerRoute(/^http[s]?:\/\/fonts.gstatic.com\/(.*)/, workbox.strategies.staleWhileRevalidate(), 'GET');
+
 var DDOPlanner = ( function () {
 	let dataStore = {};
 
